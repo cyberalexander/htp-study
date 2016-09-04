4b3a674cfe97ca30eda2ce3484b9d121ae6a211f
CyberAlexander <festivdom@gmail.com>
2016-09-04 22:07:53 +0300

    commit 04.09.2016 22:07

---
+++ script/main-study.js
@@ -0,0 +1,36 @@
+/**
+ * Created by alexanderleonovich on 03.09.16.
+ */
+//alert("Hello World!");
+var Rost=172.5;
+var RostStr = "2";
+var HelloMsg='Привет';
+var Name='умник';
+
+console.log( Rost*2 );
+console.log( Rost-172 );
+console.log( (Rost-0.5)/2 );
+console.log( HelloMsg + ' ' + Name );
+//console.log(RostStr * Rost);
+var a = '8';
+a*=2
+console.log(a);
+
+// var HS=prompt('Введите ваш рост:');
+// var H=parseFloat(HS);
+// var H1=parseInt(HS);
+// console.log(H);
+// console.log(H1);
+// console.log( 'Ваш рост - ' + (H/2.54) + ' дюймов' );
+// var el = document.getElementsByClassName('zuz');
+// console.log(el);
+// console.log(el);
+
+
+/*var SpeedS=prompt('Введите скорость:');
+var Speed=parseFloat(SpeedS);
+
+if ( Speed>60 )
+    console.log('превышение!');
+else
+    console.log('норма.');*/
